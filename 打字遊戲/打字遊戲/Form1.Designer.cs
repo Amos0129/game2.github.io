46b61674afa8e5d218cc1b45cb6af56ff17740f0
Amos0129 <110878143+Amos0129@users.noreply.github.com>
2022-08-10 02:10:56 +0800

    version1

---
+++ 打字遊戲/打字遊戲/Form1.Designer.cs
@@ -0,0 +1,333 @@
+﻿namespace 打字遊戲
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.timer4 = new System.Windows.Forms.Timer(this.components);
+            this.progressBar2 = new System.Windows.Forms.ProgressBar();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label9 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(4, 2);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(6);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(2440, 1328);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.Location = new System.Drawing.Point(15, 15);
+            this.button1.Margin = new System.Windows.Forms.Padding(6);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(150, 46);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "開始";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(273, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(124, 24);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "敵方生命值：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Impact", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(339, 145);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(129, 48);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Impact", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(564, 145);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(130, 48);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "label3";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(391, 15);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(567, 23);
+            this.progressBar1.TabIndex = 5;
+            this.progressBar1.Value = 100;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Impact", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(785, 145);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(129, 48);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "label4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(24, 255);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(67, 24);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "分數：";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(95, 255);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(21, 24);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "0";
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 1000;
+            this.timer2.Tick += new System.EventHandler(this.Timer2_Tick);
+            // 
+            // timer3
+            // 
+            this.timer3.Tick += new System.EventHandler(this.Timer3_Tick);
+            // 
+            // timer4
+            // 
+            this.timer4.Tick += new System.EventHandler(this.Timer4_Tick);
+            // 
+            // progressBar2
+            // 
+            this.progressBar2.Location = new System.Drawing.Point(391, 553);
+            this.progressBar2.Name = "progressBar2";
+            this.progressBar2.Size = new System.Drawing.Size(567, 23);
+            this.progressBar2.TabIndex = 12;
+            this.progressBar2.Value = 100;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Transparent;
+            this.button2.Location = new System.Drawing.Point(16, 145);
+            this.button2.Margin = new System.Windows.Forms.Padding(6);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(150, 46);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "暫停";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(276, 550);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(124, 24);
+            this.label9.TabIndex = 15;
+            this.label9.Text = "我方生命值：";
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Transparent;
+            this.button3.Location = new System.Drawing.Point(15, 73);
+            this.button3.Margin = new System.Windows.Forms.Padding(6);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(150, 46);
+            this.button3.TabIndex = 14;
+            this.button3.Text = "結束遊戲";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(368, 133);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(70, 70);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox2.TabIndex = 16;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(596, 133);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(70, 70);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox3.TabIndex = 17;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4.Image")));
+            this.pictureBox4.Location = new System.Drawing.Point(811, 133);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(70, 70);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox4.TabIndex = 18;
+            this.pictureBox4.TabStop = false;
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Transparent;
+            this.button4.Location = new System.Drawing.Point(15, 203);
+            this.button4.Margin = new System.Windows.Forms.Padding(6);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(150, 46);
+            this.button4.TabIndex = 19;
+            this.button4.Text = "繼續";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.Button4_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(15, 366);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(215, 208);
+            this.textBox1.TabIndex = 20;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 339);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(67, 24);
+            this.label6.TabIndex = 21;
+            this.label6.Text = "排行榜";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1096, 624);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.progressBar2);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.KeyPreview = true;
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.Timer timer4;
+        private System.Windows.Forms.ProgressBar progressBar2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ 打字遊戲/打字遊戲/Form1.cs
@@ -0,0 +1,364 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace 打字遊戲
+{
+    public partial class Form1 : Form
+    {
+        Random rd = new Random(); //宣告亂數
+        Random rdcol = new Random(); //宣告顏色亂數
+        Color[] col = new Color[3]; //三種顏色
+        FileInfo fi;  //宣告建檔
+        StreamWriter sw; //寫檔
+
+        int score; //分數
+        int tmp; //排序暫存
+        int Num = 1; //幾人
+        int clik = 0, cliktrue = 0; //按錯及沒按錯
+        int[,] ch = new int[3, 4]; //宣告陣列
+
+        public Form1()
+        {
+            InitializeComponent(); //初始化
+
+            label6.Visible = false;  //排行榜隱藏
+            textBox1.Visible = false; 
+
+            pictureBox2.Visible = false; //字母隱藏
+            label2.Visible = false; //圖片隱藏
+
+            pictureBox3.Visible = false;
+            label3.Visible = false; //圖片隱藏
+
+            pictureBox4.Visible = false; //字母隱藏
+            label4.Visible = false; //圖片隱藏
+
+            col[0] = Color.White; //陣列第一個白色
+            col[1] = Color.Gold; //陣列第二個金色
+            col[2] = Color.GreenYellow; //陣列第三個黃綠色
+        }
+
+        void reset() //重設
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                ch[i, 0] = rd.Next(33, 127);
+                ch[i, 1] = rd.Next(50, 90);
+                ch[i, 2] = 0; //有沒有打到
+            }
+
+            label7.Text = "0";
+            score = 0;
+            progressBar1.Value = 100;
+            progressBar2.Value = 100;
+        }
+
+        void ASCII()
+        {
+            if (clik > 3)
+            {
+                return;
+            }
+
+            ch[clik, 0] = rd.Next(33, 127);
+            ch[clik, 1] = rd.Next(50, 90);
+            ch[clik, 2] = 0; //有沒有打到
+
+            if (clik == 0)
+            {
+                pictureBox2.Top = ch[0, 1];
+                label2.Text = ((char)ch[0, 0]).ToString();
+            }
+
+            if (clik == 1)
+            {
+                pictureBox3.Top = ch[1, 1];
+                label3.Text = ((char)ch[1, 0]).ToString();
+            }
+
+            if (clik == 2)
+            {
+                pictureBox4.Top = ch[2, 1];
+                label4.Text = ((char)ch[2, 0]).ToString();
+            }
+        }
+
+        void start()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                clik = i;
+                ASCII();
+            }
+
+            pictureBox2.Visible = true;
+            label2.Visible = true;
+
+            pictureBox3.Visible = true;
+            label3.Visible = true;
+
+            pictureBox4.Visible = true;
+            label4.Visible = true;
+
+            timer1.Enabled = true;
+            timer2.Enabled = true;
+            timer3.Enabled = true;
+            timer4.Enabled = true;
+
+            button4.Visible = false;
+            label6.Visible = false;
+            textBox1.Visible = false;
+            reset();          
+        }
+
+        void PHOTO()
+        {
+            pictureBox1.Image = Image.FromFile("gamephoto\\background.jpg");
+
+            TP(label6);
+            TP(label1);
+            TP(label5);
+            TP(label7);
+            TP(label9);
+
+            TP2(pictureBox2,label2);
+            TP2(pictureBox3,label3);
+            TP2(pictureBox4,label4);
+        }
+
+        void save()
+        {
+            fi = new FileInfo("score.txt");
+            sw = fi.CreateText();
+
+            for (int i = 0; i < ch[i,3]; i++)
+            {
+                sw.WriteLine(ch[i, 3].ToString());
+            }
+
+            sw.Flush();
+            sw.Close();
+        }
+
+        void TP(Label a)
+        {
+            a.BackColor = Color.Transparent;
+            pictureBox1.Controls.Add(a);
+        }
+
+        void TP2(PictureBox c, Label b)
+        {
+            c.BackColor = Color.Transparent;
+            pictureBox1.Controls.Add(c);
+            b.BackColor = Color.Transparent;
+            b.Location = new Point(16, 10);
+            c.Controls.Add(b);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            PHOTO();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            start();
+            button1.Visible = false;
+        }   
+
+        private void Timer2_Tick(object sender, EventArgs e)
+        {
+            if (progressBar1.Value == 0)
+            {
+                gameover();
+            }
+
+            if(progressBar2.Value - 1 > 0)
+            {
+                progressBar2.Value -= 1;
+            }
+            else
+            {
+                gameover();
+            }
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            txt(pictureBox2, label2);
+        }
+
+        private void Timer3_Tick(object sender, EventArgs e)
+        {
+            txt(pictureBox3, label3);
+        }
+
+        private void Timer4_Tick(object sender, EventArgs e)
+        {
+            txt(pictureBox4, label4);
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                switch (i)
+                {
+                    case 0:
+                        aaaa(label2, i,e.KeyChar.ToString());
+                        break;
+
+                    case 1:
+                        aaaa(label3, i, e.KeyChar.ToString());
+                        break;
+
+                    case 2:
+                        aaaa(label4, i, e.KeyChar.ToString());
+                        break;
+                }
+            }
+
+            if (cliktrue == 0)
+            {
+                MISS(clik);
+
+                if (progressBar1.Value <= 0)
+                {
+                    gameover();
+                    reset();
+                    return;
+                }
+            }
+            cliktrue = 0;
+        }
+
+        void txt(PictureBox p, Label l)
+        {
+            if (p.Top + 5 > this.ClientSize.Height - 40) //失誤
+            {
+                clik = 0;
+                progressBar2.Value = 0;
+                gameover();
+                save();
+            }
+            else
+            {
+                p.Top += 5;
+            }
+        }
+
+        void aaaa(Label aaaaa, int i,string aaaaaa)
+        {
+            if (aaaaaa == aaaaa.Text)
+            {
+                MissOrNot(i);
+                aaaaa.ForeColor = col[rdcol.Next(0, 3)];
+                cliktrue ++;
+            }
+        }
+
+        void gameover() //結束遊戲
+        {
+            ch[Num, 3] = score;
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (ch[i, 3] > ch[j, 3])
+                    {
+                        tmp = ch[i, 3];
+                        ch[i, 3] = ch[j, 3];
+                        ch[j, 3] = tmp;
+                    }
+                }
+            }
+
+            textBox1.Clear();
+            for (int i = 0; i < Num; i++)
+            {
+                textBox1.AppendText("玩家 " + (i + 1).ToString() + "：" + ch[i, 3].ToString() + "分\r\n");
+            }
+            Num++;
+
+            timer1.Enabled = false;
+            timer2.Enabled = false;
+            timer3.Enabled = false;
+            timer4.Enabled = false;
+
+            label6.Visible = true;         
+            button1.Visible = true;
+            textBox1.Visible = true;
+            MessageBox.Show("GameOver");           
+            return;
+        }
+
+        private void Button2_Click(object sender, EventArgs e) //暫停
+        {
+            timer1.Enabled = false;
+            timer2.Enabled = false;
+            timer3.Enabled = false;
+            timer4.Enabled = false;
+            button2.Visible = false;
+            button4.Visible = true;
+        }
+
+        private void Button4_Click(object sender, EventArgs e) //繼續
+        {
+            timer1.Enabled = true;
+            timer2.Enabled = true;
+            timer3.Enabled = true;
+            timer4.Enabled = true;
+            button2.Visible = true;
+            button4.Visible = false;
+        }
+
+        void MISS(int i) //按錯
+        {
+            score -= 5;
+            ch[i, 2] = 0;
+            label7.Text = score.ToString();
+
+            if (progressBar2.Value - 3 > 0)
+            {
+                progressBar2.Value -= 3;
+            }
+            else
+            {
+                progressBar2.Value = 0;
+                gameover();
+            }
+        }
+
+        void MissOrNot(int i) //有按對功能
+        {
+            clik = i;
+            score += 20;
+            ch[i, 2] = 1;
+            label7.Text = score.ToString();
+
+            if (progressBar1.Value - 3 > 0)
+            {
+                progressBar1.Value -= 3;
+            }
+            else
+            {
+                progressBar1.Value = 0;
+                gameover();
+            }
+            ASCII();
+        }
+    }
+}
